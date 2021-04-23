0f20d6c51ee1551959079f3c4891dc492774893a
mbmassey <mbmassey@stanford.edu>
2021-04-23 16:00:40 -0700

    user can now iterate through entire image dataset, and exit at will

--- selectGlitches.py
+++ selectGlitches.py
@@ -3,8 +3,10 @@ import pandas as pd
 import os
 import numpy as np
 import math
+import re
 
-def insert_row(x_coor, y_coor, radius, df)
+def insert_row():
+    global df
     ds = pd.Series(dtype = 'float64')
     ds['x'] = int(x_coor)
     ds['y'] = int(y_coor)
@@ -12,7 +14,7 @@ def insert_row(x_coor, y_coor, radius, df)
     df = df.append(ds, ignore_index=True)
 
 def draw_circle(event, x, y, flags, param):
-    global x1, y1, x_coor, y_coor, radius, img, windowName, df
+    global x1, y1, radius, img
     if event == cv2.EVENT_LBUTTONDOWN:
         x1, y1, = x, y
         print("Button down")
@@ -20,14 +22,16 @@ def draw_circle(event, x, y, flags, param):
         radius = math.hypot(x - x1, y - y1)
         cv2.circle(img, (x1,y1), int(radius), (255, 0, 255), 10)
         cv2.imshow(windowName, img)
-        insert_row(x_coor, y_coor, radius, df)
+        insert_row()
         print("Button up")
 
-def analyze_electrode(file_path, x_coor, y_coor, df): 
+def analyze_electrode(file_path): 
+    global escpress 
+    escpress = False
     rkey = ord('r')
     nextkey = ord('n')
     esckey = 27
-    
+    global img, windowName
     original = cv2.imread(file_path)
     img = original.copy()
     windowName = 'Drawing'
@@ -38,6 +42,7 @@ def analyze_electrode(file_path, x_coor, y_coor, df):
         key = cv2.waitKey(20) & 0xFF
         if key == esckey:
             cv2.destroyAllWindows()
+            escpress = True
             break
         elif key == rkey:
             img = original.copy()
@@ -52,8 +57,13 @@ if __name__ == "__main__":
     regex = re.compile(r'^y(\d+)_x(\d+)\.JPG$')
     filenames = os.listdir(indir)
     file_list = [f for f in map(regex.match, filenames) if f is not None]
+    global df, x_coor, y_coor
+
     df = pd.DataFrame()
     for f in file_list:
         curr_file = indir + f.group(0)
-        analyze_electrode(curr_file, f.group(2), f.group(1), df)
+        x_coor, y_coor = f.group(2), f.group(1)
+        analyze_electrode(curr_file)
+        if escpress == True:
+            break
 
